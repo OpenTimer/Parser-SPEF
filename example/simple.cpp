050653ac5b5c4ebd9b5d5815c7b4f5e4405290b0
clin99 <clin99@csl-408-12.csl.illinois.edu>
2018-08-08 17:09:37 -0500

    Update example

--- example/simple.cpp
+++ example/simple.cpp
@@ -1,12 +1,23 @@
 
 #include "spefparser.hpp"  // the only include you need
 
-int main(){
+int main(int argc, char* argv[]){
+  if(argc != 2){
+    std::cout << "Usage: ./simple [spef_file]";
+    return 1;
+  }
+
+  if(not std::experimental::filesystem::exists(argv[1])){
+    std::cout << "Cannot found " <<  argv[1] << '\n';
+    return 1;
+  }
+
   // Create a data to be filled by the parser
   spef::Spef data;
 
-  // Invoke the parse function and check the return value
-  if(spef::parse_spef("./simple.spef", data)){
+  // Invoke the parse_spef function and check the return value
+  if(spef::parse_spef_file(argv[1], data)){
+    // Call dump function to see the content
     std::cout << data.dump() << '\n';
   }
 }
