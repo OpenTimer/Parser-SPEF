0500b49c16ac0345726e93a8509ee168f4eaab79
clin99 <clin99@csl-408-12.csl.illinois.edu>
2018-08-13 10:19:29 -0500

    Rename spefparser to parser-spef

--- example/simple.cpp
+++ example/simple.cpp
@@ -1,7 +1,24 @@
 
-#include "spefparser.hpp"  // the only include you need
+#include "parser-spef.hpp"
 
 int main(int argc, char* argv[]){
+  //{
+  //  spef::Spef parser;
+  //  parser.parse_spef_file("/home/clin99/Software/spirit/aes_cipher_top.spef");
+  //  //parser.parse_spef_file("/home/clin99/Software/Parser-SPEF/temp/leon2_iccad.spef");
+  //  parser.name_expansion();
+  //  //auto origin = parser.dump();
+  //  //for(auto &n : parser.nets){
+  //  //  parser.name_expansion(n);
+  //  //}
+  //  //for(auto &p : parser.ports){
+  //  //  parser.name_expansion(p);
+  //  //}
+  //  //assert(origin == parser.dump());
+  //  //std::cout << "nof nets = " << parser.nets.size() << "\n";
+  //  std::cout << parser.dump() << "\n";
+  //  return 0;
+  //}
 
   if(argc != 2){
     std::cout << "Usage: ./simple [spef_file]";
@@ -16,9 +33,19 @@ int main(int argc, char* argv[]){
   // Create a data to be filled by the parser
   spef::Spef data;
 
+  //auto buffer {spef::file_to_memory(argv[1])};
+  //std::vector<std::string_view> vec;
+  //spef::split_on_space(buffer.data(), &(buffer.data()[buffer.size()-1]), vec);
+  //for(const auto& v: vec){
+  //  std::cout << v << '\n';
+  //}
+  //exit(1);
+
+
   // Invoke the parse_spef function and check the return value
-  if(data.parse_spef_file(argv[1])){
+  if(data.read(argv[1])){
     // Call dump function to see the content
+    data.name_expansion();
     //std::cout << data.dump() << '\n';
   }
 }
